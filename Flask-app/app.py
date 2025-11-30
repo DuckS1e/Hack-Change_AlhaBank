62f76d4efeb3b81a9f8c8bd41e63078a7439d668
MsxSerdyukov <151764241+MsxSerdyukov@users.noreply.github.com>
2025-11-30 16:25:09 +0300

    Небольшой веб

--- Flask-app/app.py
+++ Flask-app/app.py
@@ -25,7 +25,7 @@ def index():
     return redirect(url_for('login'))
 
 @app.route('/registration', methods=['GET', 'POST'])
-def registration():
+def register():
     if request.method == 'POST':
         username = request.form['username']
         email = request.form['email']
@@ -33,7 +33,7 @@ def registration():
         confirmation = request.form['confirmation']
         if password != confirmation:
             flash('Пароли не совпали twin')
-            return render_template('reg.html')
+            return render_template('register.html')
 
         hash_pass = generate_password_hash(password)
 
@@ -48,29 +48,35 @@ def registration():
         except:
             flash('Такой brototype уже есть')
 
-        return render_template('reg.html')
+    return render_template('register.html')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get('username', '')
+        password = request.form.get('password', '')
+
+        if not username or not password:
+            flash('Заполните все поля!')
+            return render_template('login.html')
+
         conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-        c.execute("""SELECT * FROM users WHERE USERNAME = ?""", (username,))
-        user = c.fetchone()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        user = cursor.fetchone()
         conn.close()
 
         if user and check_password_hash(user[3], password):
             session['user_id'] = user[0]
             session['username'] = user[1]
-            session['email'] = user[2]
-            flash('Вход выполнен успешно')
+            flash('Вход выполнен успешно!')
             return redirect(url_for('index'))
         else:
-            flash('Ты ошибся cuh')
+            flash('Неверное имя пользователя или пароль!')
+            return render_template('login.html')
 
-        render_template('log.html')
+    return render_template('login.html')
 
 
 @app.route('/logout')
