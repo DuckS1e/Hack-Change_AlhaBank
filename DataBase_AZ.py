c3d61738adbc073103ba9689be6e91595add9265
DuckS1e <alex_firebatrus@mail.ru>
2025-12-03 15:35:14 +0300

    Включил красивый вывод, добавил цвета

--- DataBase_AZ.py
+++ DataBase_AZ.py
@@ -1,7 +1,11 @@
 import pandas as pd
 from pathlib import Path
-from rich.console import Console
-from rich.table import Table
+from colorama import init
+from tabulate import tabulate
+init()
+from colorama import Fore, Back, Style
+
+
 
 # Определяем путь
 file_path_features = Path('DataBase_csvs') / 'features_description.csv'
@@ -9,9 +13,25 @@ file_path_income_test = Path('DataBase_csvs') / 'hackathon_income_test.csv'
 file_path_income_train = Path('DataBase_csvs') / 'hackathon_income_train.csv'
 file_path_sample_submission = Path('DataBase_csvs') / 'sample_submission.csv'
 
-# Читаем файл
+# Читаем и делаем вывод features_description.csv
+print(Fore.GREEN + "_"*35)
+print('DataFrame_Features')
+print("_"*35 + Style.RESET_ALL)
 df_features = pd.read_csv(file_path_features, sep=';', encoding='windows-1251')
+print(tabulate(df_features.head(), headers=df_features.columns, tablefmt='pretty'))
+
+# Читаем и делаем вывод hackathon_income_test.csv
+print(Fore.GREEN + "_"*35)
+print('DataFrame_Test')
+print("_"*35 + Style.RESET_ALL)
 df_test = pd.read_csv(file_path_income_test, sep=';', encoding='utf-8')
+print(tabulate(df_test.head(), headers=df_test.columns, tablefmt='pretty'))
+
+# Читаем и делаем вывод sample_submission.csv
+print(Fore.GREEN + "_"*35)
+print('DataFrame_Test')
+print("_"*35 + Style.RESET_ALL)
 df_sample_submission = pd.read_csv(file_path_sample_submission, sep=';', encoding='utf-8')
+print(tabulate(df_sample_submission.head(), headers=df_sample_submission.columns, tablefmt='pretty'))
 
 
