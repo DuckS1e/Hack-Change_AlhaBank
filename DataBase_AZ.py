df98c8a395d2e8b4cb4afb6ccbaf20afa7aed1b7
DuckS1e <alex_firebatrus@mail.ru>
2025-12-03 13:16:26 +0300

    Добавил комментарии. Провел анализ данных

--- DataBase_AZ.py
+++ DataBase_AZ.py
@@ -14,6 +14,7 @@ file_path_income_train = Path('DataBase_csvs') / 'hackathon_income_train.csv'
 file_path_sample_submission = Path('DataBase_csvs') / 'sample_submission.csv'
 
 # Читаем и делаем вывод features_description.csv
+# Крч в features объясняется что каждый столбец в самих датасетах значат
 print(Fore.GREEN + "_"*35)
 print('DataFrame_Features')
 print("_"*35 + Style.RESET_ALL)
@@ -21,6 +22,7 @@ df_features = pd.read_csv(file_path_features, sep=';', encoding='windows-1251')
 print(tabulate(df_features.head(), headers=df_features.columns, tablefmt='pretty'))
 
 # Читаем и делаем вывод hackathon_income_test.csv
+# Сам датафрейм, куча значений. Нужно вычленять лишние
 print(Fore.GREEN + "_"*35)
 print('DataFrame_Test')
 print("_"*35 + Style.RESET_ALL)
@@ -28,6 +30,7 @@ df_test = pd.read_csv(file_path_income_test, sep=';', encoding='utf-8')
 print(tabulate(df_test.head(), headers=df_test.columns, tablefmt='pretty'))
 
 # Читаем и делаем вывод sample_submission.csv
+# Либо это пример вывода, либо константая модель какая-то
 print(Fore.GREEN + "_"*35)
 print('DataFrame_Test')
 print("_"*35 + Style.RESET_ALL)
