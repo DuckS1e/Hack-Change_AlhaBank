883bb0bca20f0d728d9987215d385e68656aa771
DuckS1e <alex_firebatrus@mail.ru>
2025-12-08 01:02:31 +0300

    Подправил вывод. Удалил мусор

--- DataBase_AZ.py
+++ DataBase_AZ.py
@@ -9,68 +9,25 @@ from colorama import Fore, Back, Style
 
 
 # Определяем путь
-
-file_path_features = Path('DataBase_csvs') / 'features_description.csv'
-file_path_income_test = Path('DataBase_csvs') / 'hackathon_income_test.csv'
-file_path_income_train = Path('DataBase_csvs') / 'hackathon_income_train.csv'
-file_path_sample_submission = Path('DataBase_csvs') / 'sample_submission.csv'
-
-
-
-# Читаем и делаем вывод features_description.csv
-# Крч в features объясняется что каждый столбец в самих датасетах значат
-
-print(Fore.GREEN + "_"*35)
-print('DataFrame_Features')
-print("_"*35 + Style.RESET_ALL)
-df_features = pd.read_csv(file_path_features, sep=';', encoding='windows-1251')
-print(tabulate(df_features.head(), headers=df_features.columns, tablefmt='pretty'))
-print(Fore.LIGHTYELLOW_EX + f"Shape: {df_features.shape}" + Style.RESET_ALL)
+DATA_DIR = Path('DataBase_csvs')
+file_paths = {
+    'features': DATA_DIR / 'features_description.csv',
+    'train': DATA_DIR / 'hackathon_income_train.csv',
+    'test': DATA_DIR / 'hackathon_income_test.csv',
+    'submission': DATA_DIR / 'sample_submission.csv'
+}
 
 
-# Читаем и делаем вывод hackathon_income_train.csv
-# Сам датафрейм, куча значений. Нужно вычленять лишние
+def load_dataframe(name, path, encoding):
+    try:
+        df = pd.read_csv(path, sep=';', encoding=encoding)
+        print(Fore.GREEN + f"Данные загружены: {name.capitalize()}" + Style.RESET_ALL + s)
+        return df
+    except Exception as e:
+        print(Fore.RED + f"Не удалось загрузить данные {name.capitalize()}: {e}" + Style.RESET_ALL)
+        return None
 
-print(Fore.GREEN + "_"*35)
-print('DataFrame_train')
-print("_"*35 + Style.RESET_ALL)
-df_train = pd.read_csv(file_path_income_train, sep=';', encoding='utf-8')
-print(tabulate(df_train.head(), headers=df_train.columns, tablefmt='pretty'))
-print(Fore.LIGHTYELLOW_EX + f"Shape: {df_train.shape}" + Style.RESET_ALL)
-
-
-# Читаем и выводим hackaton_income_test.csv
-# Отдельный тествый датасет для проверки модели
-
-print(Fore.GREEN + "_"*35)
-print('DataFrame_test')
-print("_"*35 + Style.RESET_ALL)
-df_test = pd.read_csv(file_path_income_test, sep=';', encoding='utf-8')
-print(tabulate(df_test.head(), headers=df_test.columns, tablefmt='pretty'))
-print(Fore.LIGHTYELLOW_EX + f"Shape: {df_test.shape}" + Style.RESET_ALL)
-
-
-
-# Читаем и делаем вывод sample_submission.csv
-# Данный датасет надо менять под target, который мы сами получили.
-
-print(Fore.GREEN + "_"*35)
-print('DataFrame_Sample_Submission')
-print("_"*35 + Style.RESET_ALL)
-df_sample_submission = pd.read_csv(file_path_sample_submission, sep=';', encoding='utf-8')
-print(tabulate(df_sample_submission.head(), headers=df_sample_submission.columns, tablefmt='pretty'))
-print(Fore.LIGHTYELLOW_EX + f"Shape: {df_sample_submission.shape}" + Style.RESET_ALL)
-
-FEATURES = df_train.columns[2:]
-
-# Убираем Features из df_train, количество пустых ячеек которых больше 50%
-
-def remove_sparse_columns(df):
-    for feature in FEATURES:
-        column = df[feature]
-        if column.isnull().sum() > len(column) * 0.2:
-            df.drop(feature, axis=1)
-    return df
-
-df_train = remove_sparse_columns(df_train)
-print(df_train.head())
+df_features = load_dataframe('features', file_paths['features'], 'windows-1251')
+df_train = load_dataframe('train', file_paths['train'], 'utf-8')
+df_test = load_dataframe('test', file_paths['test'], 'utf-8')
+df_submission = load_dataframe('submission', file_paths['submission'], 'utf-8')
--- maksik.py
+++
@@ -1,2 +0,0 @@
-print('Выборнов лучший real nigger')
-print("OBJECTION: он белый с маленьким членом")
